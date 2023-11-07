a4c7eda18ce7d646e5f3e4365e405e089038d647
Alexis Comte <alexis.comte73@icloud.com>
2023-11-07 13:40:48 +0100

    fix checkbox

--- presentation/components/checkbox/checkbox.tsx
+++ presentation/components/checkbox/checkbox.tsx
@@ -1,4 +1,5 @@
 import { CheckIcon } from '@/presentation/@shared/assets'
+import { useRef } from 'react'
 
 export function CheckBox ({
   checked = false,
@@ -9,17 +10,27 @@ export function CheckBox ({
   name: string
   onChange: () => void
 }) {
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const handleClick = () => {
+    if (!inputRef.current) return
+    inputRef.current.click()
+  }
   return (
     <div className='relative h-4 w-4'>
       <input
         onChange={onChange}
+        ref={inputRef}
         id={name}
         type='checkbox'
         checked={checked}
         className={`h-4 w-4 appearance-none bg-white border-[1px] border-Medium-Grey border-opacity-25 rounded-sm outline-none checked:bg-Main-Purple`}
-      ></input>
+      />
       {checked && (
-        <CheckIcon className='absolute text-[12px] top-[3px] left-[2px]' />
+        <CheckIcon
+          onClick={handleClick}
+          className='absolute text-[12px] top-[3px] left-[2px]'
+        />
       )}
     </div>
   )
