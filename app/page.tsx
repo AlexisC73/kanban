253592a8077518a757d3c21c326a56751223485f
Alexis Comte <alexis.comte73@icloud.com>
2023-11-10 17:02:27 +0100

    fix import

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
 'use client'
 
-import { NoTaskScreen } from './no-task-screen/NoTaskScreen'
-
 export default function Home () {
-  return (
-    <main className='flex items-center justify-center w-full'>
-      <NoTaskScreen />
-    </main>
-  )
+  return <main className='flex items-center justify-center w-full p-6'></main>
 }
