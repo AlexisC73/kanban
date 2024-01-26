5a9cfbf6995b89e8a81e2fabe662a1b011ef1259
Alexis Comte <alexis.comte73@icloud.com>
2024-01-26 11:20:54 +0100

    switch text for fake auth

--- presentation/components/SignInModal/SignInForm/SignInForm.tsx
+++ presentation/components/SignInModal/SignInForm/SignInForm.tsx
@@ -37,7 +37,7 @@ export const SignInWithGithubButton = () => {
       className='py-2 px-4 max-w-md flex gap-x-2 justify-center items-center bg-gray-600 hover:bg-gray-700 focus:ring-gray-500 focus:ring-offset-gray-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg'
     >
       <GithubIcon className='text-[22px]' />
-      Sign in with GitHub
+      Sign in Fake
     </button>
   )
 }
